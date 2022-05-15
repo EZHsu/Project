c7ea5fa446ba71a1eac29b4c44c69bb785160b0e
EZHsu <101862861+EZHsu@users.noreply.github.com>
2022-05-16 00:07:58 +0800

    Update Report.sql

--- SQLs/Report.sql
+++ SQLs/Report.sql
@@ -16,7 +16,7 @@ CREATE TABLE `Reports` (
   
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Reports` (`Name`, `Category`, `ID`, `Date`, `Status`, `Detail`, ) VALUES
+INSERT INTO `Reports` (`Name`, `Category`, `ID`, `Date`, `Status`, `Detail` ) VALUES
 ('王小明', '介面與網頁問題', '00001', '2022-05-08 14:20:33', '未處理', '無法傳送訊息'),
 ('李相赫', '網路延遲與連接問題', '00002','2022-05-05 04:21:10' , '已處理', '網站35ping回應速度慢'),
 ('Chris Paul', '客服問題', '00003', '2022-05-01 18:29:19', '已處理', '沒有立刻得到客服回應');
