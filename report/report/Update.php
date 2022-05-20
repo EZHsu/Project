f95f109bc30390d0b9b2a20e3c14f535a5705244
EZHsu <101862861+EZHsu@users.noreply.github.com>
2022-05-20 12:35:40 +0800

    Update Update.php

--- report/report/Update.php
+++ report/report/Update.php
@@ -9,10 +9,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-$name = $_POST['name'];
+}
 
-$sql = "UPDATE costomer_service SET end= 1 WHERE name = '$name'";
+$key = isset($_GET['name']) ? $_GET['name'] : '';
+
+$sql = "UPDATE costomer_service SET end= 1 WHERE name = '$key'";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record updated successfully";
